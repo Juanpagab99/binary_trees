337ee17d7a9b85e0e5a3d453c7b6f999b248e4f9
Juanpagab99 <juanpagab99@gmail.com>
2021-07-21 11:35:56 +0700

    task 3 update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,17 +8,11 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree == NULL){
+	if (tree == NULL)
 		return;
-	}
-	
-	if (tree->left != NULL){
+	if (tree->left != NULL)
 		binary_tree_delete(tree->right);
-	}
-
-	if (tree->right != NULL){
+	if (tree->right != NULL)
 		binary_tree_delete(tree->left);
-	}
-
 	free(tree);
 }
