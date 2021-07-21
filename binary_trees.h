9d5078e81bb7551ba926c3e2f8fbc146a4abc5d5
SrDri <juanjcarabali@gmail.com>
2021-07-21 22:51:54 +0700

    Update task 10

--- binary_trees.h
+++ binary_trees.h
@@ -47,5 +47,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t find_height(const binary_tree_t *tree, size_t h);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H_ */
