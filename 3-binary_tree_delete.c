8a7306c4e9f2b3b3586bee71b758a886b05c07c6
SrDri <juanjcarabali@gmail.com>
2021-07-21 10:43:51 +0700

    Update task 3 - delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete binary tree
+ * @tree: root tree
+ * Return: void
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL){
+		return;
+	}
+	
+	if (tree->left != NULL){
+		binary_tree_delete(tree->right);
+	}
+
+	if (tree->right != NULL){
+		binary_tree_delete(tree->left);
+	}
+
+	free(tree);
+}
